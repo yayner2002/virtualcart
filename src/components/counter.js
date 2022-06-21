8d84b7bd03ef556aae0445960dcb83ea31aa5361
yayner <yayner2002@gmail.com>
2022-06-21 13:51:09 +0300

    EMbedding expressions

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return (
-            <h1>Hello World</h1>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
