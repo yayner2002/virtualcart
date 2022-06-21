b55e3184be295818b60bbee92f5553fd0e74fb59
yayner <yayner2002@gmail.com>
2022-06-21 14:01:48 +0300

    Embedding expressions and object destructuring

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+    state = {
+        count: 0
+    }
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+  formatCount() {
+    const { count } = this.state
+    return count === 0 ? 'zero' : count
   }
 }
 
