d0505bd75624da585c91aec83d6a0100321993e1
yayner <yayner2002@gmail.com>
2022-06-24 13:47:26 +0300

    Refactor code

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,23 +2,28 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 10,
   };
 
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
+  }
+
   render() {
+    let classes = "badge m-2 bg-";
+    classes += ( this.state.count === 0 ) ? "warning" : "success";
+
     return (
       <React.Fragment>
-        <span className="badge bg-success m-2">{this.formatCount()}</span>
+        <span className= {classes}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">
           Increment
         </button>
       </React.Fragment>
     );
   }
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "zero" : count;
-  }
+
 }
 
 export default Counter;
