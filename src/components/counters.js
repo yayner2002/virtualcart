1b4f9cd4574587530890659d1c9c14ca301cb09a
yayner <yayner2002@gmail.com>
2022-06-24 16:42:49 +0300

    single source of truth

---
+++ src/components/counters.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    items: [
+      {
+        id: 1,
+        value: 0,
+      },
+      {
+        id: 2,
+        value: 40,
+      },
+      {
+        id: 3,
+        value: 0,
+      },
+      {
+        id: 4,
+        value: 0,
+      },
+    ],
+  };
+  handleReset = () => {
+    const items = this.state.items.map(item => {
+        item.value = 0
+        return item
+    })
+    this.setState({items})
+  }
+  handleDelete = (deletedId) => {
+    const itemsAfterDelete = this.state.items.filter(
+      (item) => item.id !== deletedId
+    );
+    this.setState({ items: itemsAfterDelete });
+  };
+  render() {
+    return (
+      <div>
+        <div className="button btn btn-primary m-2 btn-sm" onClick={this.handleReset}>Reset</div>
+        {this.state.items.map((counter) => (
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onDelete={this.handleDelete}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
