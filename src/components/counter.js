807c82f4c0ad3dee8713e215c7c7442da9bd1854
yayner <yayner2002@gmail.com>
2022-06-24 14:22:52 +0300

    handling events

--- src/components/counter.js
+++ src/components/counter.js
@@ -6,23 +6,26 @@ class Counter extends Component {
     items: ['item 1', 'item 2', 'item 3', 'item 4']
   };
 
-  // formatCount() {
-  //   const { count } = this.state;
-  //   return count === 0 ? "zero" : count;
-  // }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
+  }
   conditionalRendering() {
     if(this.state.items.length === 0) return <p>No Items to display</p>
     return <ul>{this.state.items.map(listItem => <li key={listItem}>{listItem}</li>)}</ul>
+  }
+  handleInc() {
+    console.log('button Clicked', this)
   }
 
   render() {
 
     return (
       <React.Fragment>
-        {/* <span className= {this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">
+        <span className= {this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm" onClick={this.handleInc}>
           Increment
-        </button> */}
+        </button>
         <div>
           {this.state.items.length === 0 && "Please Add new items"}
           {this.conditionalRendering()}
